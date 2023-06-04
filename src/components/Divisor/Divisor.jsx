9a4a89cf2c0e60825b0bfe7876d7ff725135dec9
Gustavo Targino <gustavo.targino03@hotmail.com>
2023-06-04 11:19:50 -0300

    FEAT / Divisor finalizado

--- src/components/Divisor/Divisor.jsx
+++ src/components/Divisor/Divisor.jsx
@@ -13,7 +13,8 @@ import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
 import Alert from '@mui/material/Alert';
-
+import Modal from '@mui/material/Modal';
+import Divider from '@mui/material/Divider';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -26,11 +27,30 @@ const MenuProps = {
   },
 };
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 const personRegex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/
+const precoRegex = /^[0-9]*$/
 const clientes = []
 const produtos = []
 
 export default function Divisor() {
+
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
 
     const[cliente, setCliente] = useState('')
     const[clienteError, setClienteError] = useState(false)
@@ -60,15 +80,22 @@ export default function Divisor() {
     const[produto, setProduto] = useState('')
     const[produtoError, setProdutoError] = useState(false)
 
+    const[preco, setPreco] = useState('')
+    const[precoError, setPrecoError] = useState(false)
+
     const[selecteds, setSelecteds] = useState([])
 
 
     const [emptyClientes, setEmptyClientesError] = useState([])
 
-    function createProductIndex(produto, consumidores) {
+
+    const[soma, setSoma] = useState(0)
+
+    function createProductIndex(produto, consumidores, valor) {
         return {
             nome: produto,
-            clientes: consumidores
+            clientes: consumidores,
+            preco: valor
         }
     }
 
@@ -76,7 +103,7 @@ export default function Divisor() {
     const handleChange = (event, index) => {
         console.log(produtos)
         const usuario = event.target.value
-        const nomeProduto = event.target.name
+        //const nomeProduto = event.target.name
         console.log(usuario)
         if(typeof usuario === 'string') {
             setSelecteds(usuario.split(','))
@@ -84,23 +111,6 @@ export default function Divisor() {
             setSelecteds(usuario)
             produtos[index].clientes = usuario
         }
-
-
-        // setSelecteds(
-        //   // On autofill we get a stringified value.
-
-        //   sl => {
-        //    if(typeof usuario === 'string') {
-        //     return{
-        //         ...sl, nome: nomeProduto, comprador: usuario
-        //     }
-                
-        //    } else {
-        //         return usuario
-        //    }
-        //   }
-        //   //typeof value === 'string' ? valor.split(',') : valor
-        // );
        
         
         
@@ -114,13 +124,18 @@ export default function Divisor() {
 
     
     function addProduct() {
-    
+        
+        if(preco.length===0 || preco < 1) {
+            setPrecoError(true)
+            return
+        }
+
         if(produto.trim()!='' && produto.length > 2) {
             setProdutoError(false)
-            produtos.push(createProductIndex(produto, []))
+            setPrecoError(false)
+            produtos.push(createProductIndex(produto, [], preco))
+            setPreco('')
             setProduto('')
-
-            
         } else {
             setProduto('')
             setProdutoError(true)
@@ -133,17 +148,32 @@ export default function Divisor() {
 
     function show() {
 
+        let soma = 0
         setEmptyClientesError([])
+        setSoma(0)
+        let exception = 0
         produtos.map((produto)=> {
             if(produto.clientes.length === 0) {
                 console.log(`${produto.nome} não possui nenhum cliente. Um produto deve ser consumido por ao menos um cliente.`)
                  setEmptyClientesError(old => [...old, `${produto.nome}`])
                 console.log(emptyClientes)
+                exception++
             } else {
                 console.log(`${produto.clientes} consumiram ${produto.nome}`)
+                soma += parseFloat(produto.preco)
             }
         })
-        
+
+        if( clientes.length!=0 && produtos.length!=0 && exception===0 ) {
+           setSoma(soma)
+           handleOpen()
+        } else {
+            console.log(clientes.length)
+            console.log(produtos.length)
+            console.log(emptyClientes.length)
+            console.log('ainda nao estamos prontos')
+            setSoma(0)
+        }
     }
 
 
@@ -174,14 +204,15 @@ export default function Divisor() {
             </Box>
 
 
-            <Box sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Box sx={{display:'flex', flexDirecion:'column' ,justifyContent:'center', alignItems:'center'}}>
                 <Box sx={{display:'flex', flexDirection:'column'}}>
                 
                 <Typography>Produto</Typography>
-             <TextField
+                <Box sx={{display:'flex', gap:1}}>
+                     <TextField
               name="produto"
               variant="outlined"
-              fullWidth
+              
               sx={{ mb: 1 }}
               onChange={(e)=> setProduto(e.target.value)}
               value={produto}
@@ -189,6 +220,23 @@ export default function Divisor() {
               helperText={produtoError ? 'Insira um produto válido' : ''}
               onKeyDown={(e)=> e.key ==='Enter' && addProduct()}
             /> 
+
+                <TextField
+                error={precoError}
+                id="standard-number"
+                label="Preço"
+                type="number"
+                value={preco}
+                sx={{width:'5rem'}}
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="standard"
+                onChange={(e)=> setPreco(e.target.value)}
+                helperText={precoError ? 'Insira um número positivo.' : ''}
+                />
+                </Box>
+            
             </Box>
                 <Button onClick={addProduct}>
                     Adicionar
@@ -197,18 +245,18 @@ export default function Divisor() {
 
              <Box sx={{display:'flex', flexDirection:'column' ,justifyContent:'flex-start', alignItems:'center'}}>
 
-            {emptyClientes.map((item)=> (
+            {emptyClientes.length > 0 &&
                 <div>
                     <Alert variant="outlined" severity="error" sx={{my: 2}}>
-                        {item} não possui clientes. Um produto deve haver clientes.
+                        {emptyClientes.join(',')} não possui clientes. Um produto deve ser consumido por ao menos um cliente.
                     </Alert>
                 </div>
-            ))}
+           }
              
 
             {produtos.map((produto, index)=> (
                 <Box key={index}>
-                    <Typography >Quem consumiu {produto.nome}?</Typography>
+                    <Typography >Quem consumiu {produto.nome}? R${produto.preco} </Typography>
                     
                  <FormControl sx={{ my:1 ,width: 300 }}>
                     <Select
@@ -236,40 +284,40 @@ export default function Divisor() {
                 
 
         <Button onClick={show}>
-                    Ver
+                    Ver conta
             </Button>
-            {/* <FormControl sx={{ m: 1, width: 300 }}>
-                    <InputLabel id="demo-multiple-checkbox-label">Produtos</InputLabel>
-                    <Select
-                    labelId="demo-multiple-checkbox-label"
-                    id="demo-multiple-checkbox"
-                    multiple
-                    value={selecteds}
-                    onChange={handleChange}
-                    input={<OutlinedInput label="Produtos" />}
-                    renderValue={(selected) => selected.join(', ')}
-                    MenuProps={MenuProps}
-                    >
-                    {clientes.map((item) => (
-                        <MenuItem key={item} value={item}>
-                        <Checkbox checked={selecteds.indexOf(item) > -1} />
-                        <ListItemText primary={item} />
-                        </MenuItem>
-                    ))}
-                    </Select>
-                </FormControl> */}
+            
             </Box>
-            {/* <TextField
-              name="decimal"
-              variant="outlined"
-              fullWidth
-              sx={{ mb: 1 }}
-              //onChange={validateNumInput}
-              //value={entradaNum}
-              //error={errorNumInput}
-              //helperText={errorNumInput ? 'O número deve ser entre 1 e 3999.' : ''}
-              //onKeyDown={(e)=> e.key ==='Enter' && arábicoParaRomano()}
-            /> */}
+          
+
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box  sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                Conta
+                </Typography>
+                {produtos.map((produto, index)=> (
+                    <>
+                <Typography key={index} id="modal-modal-description" sx={{ mt: 2 }}>
+                    {`${produto.clientes.join(', ')} ${produto.clientes.length === 1 ? 'consumiu' : 'consumiram'} ${produto.nome} de valor R$${produto.preco}. Total = A conta de ${produto.nome} se divide em R$${Math.round(parseFloat(produto.preco)/produto.clientes.length)} para cada ${produto.nome} comprado(a).  `}
+                </Typography>
+                <Divider />
+                
+                    </>
+                ))}
+
+                <Typography>Total mínimo da conta: {soma}</Typography>
+                {/* <Button>Salvar PDF</Button> */}
+               
+            </Box>
+            </Modal>
+
+
+
         </Box>
     )
 }
